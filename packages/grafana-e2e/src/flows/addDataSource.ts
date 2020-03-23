46ffe6693d0700a1792dede347aba574d1c91f5e
Steven Vachon <contact@svachon.com>
2020-03-23 09:00:57 -0400

    addDataSource accepts a config argument and checks datasource status [incomplete]

--- packages/grafana-e2e/src/flows/addDataSource.ts
+++ packages/grafana-e2e/src/flows/addDataSource.ts
@@ -1,22 +1,55 @@
 import { e2e } from '../index';
+import { fromBaseUrl, getDataSourceId } from '../support/url';
 
-export const addDataSource = (pluginName?: string): string => {
-  pluginName = pluginName || 'TestData DB';
-  e2e().logToConsole('Adding data source with pluginName:', pluginName);
+export interface DataSourceConfig {
+  alertMessage: string;
+  beforeSubmit: Function;
+  name: string;
+}
+
+const DEFAULT_DATA_SOURCE_CONFIG: DataSourceConfig = {
+  alertMessage: 'Data source is working',
+  beforeSubmit: () => {},
+  name: 'TestData DB',
+};
+
+export const addDataSource = (config: Partial<DataSourceConfig> = {}): string => {
+  const { alertMessage, beforeSubmit, name } = { ...DEFAULT_DATA_SOURCE_CONFIG, ...config };
+
+  e2e().logToConsole('Adding data source with name:', name);
   e2e.pages.AddDataSource.visit();
-  e2e.pages.AddDataSource.dataSourcePlugins(pluginName)
+  e2e.pages.AddDataSource.dataSourcePlugins(name)
     .scrollIntoView()
     .should('be.visible') // prevents flakiness
     .click();
 
-  const dataSourceName = `e2e-${new Date().getTime()}`;
+  const dataSourceName = `e2e-${Date.now()}`;
   e2e.pages.DataSource.name().clear();
   e2e.pages.DataSource.name().type(dataSourceName);
+  beforeSubmit();
   e2e.pages.DataSource.saveAndTest().click();
   e2e.pages.DataSource.alert().should('exist');
-  e2e.pages.DataSource.alertMessage().should('contain.text', 'Data source is working');
+  e2e.pages.DataSource.alertMessage().should('contain.text', alertMessage);
   e2e().logToConsole('Added data source with name:', dataSourceName);
-  e2e.setScenarioContext({ lastAddedDataSource: dataSourceName });
+
+  e2e()
+    .url()
+    .then((url: string) => {
+      const dataSourceId = getDataSourceId(url);
+
+      e2e.setScenarioContext({
+        lastAddedDataSource: dataSourceName,
+        lastAddedDataSourceId: dataSourceId,
+      });
+
+      const healthUrl = fromBaseUrl(`/api/datasources/${dataSourceId}/health`);
+      e2e().logToConsole(`Fetching ${healthUrl}`);
+      e2e()
+        .request(healthUrl)
+        .its('body')
+        .should('have.property', 'status')
+        .and('eq', 'OK');
+    });
 
   return dataSourceName;
 };
